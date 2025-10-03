89db520fea66ad14778ceb5f05bf06992286a26c
rafaelstruick <deswitaprsdeibago@gmail.com>
2025-10-03 11:01:27 +0700

    Menambahkan Prak3

---
+++ Prak3/LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kontak Kami</title>
+</head>
+<body>
+    <h1>Kontak Kami</h1>
+
+    <p>Silakan hubungi kami melalui informasi kontak fiktif di bawah ini:</p>
+    <ul>
+        <li><strong>Email:</strong> deswitaprsdeibago.com</li>
+        <li><strong>Nomor Telepon:</strong> 082285253917 </li>
+        <li><strong>Alamat:</strong> Jl. Fiktif No. 123, Kota Contoh</li>
+    </ul>
+
+    <hr>
+
+    <p>
+        <a href="/">Kembali ke Halaman Utama</a>
+    </p>
+
+    <p>
+        <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+    </p>
+
+</body>
+</html>
